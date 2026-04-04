9ae9a9fc0fe7c6a86c9b8021eb90df030f41a23c
Jason Santos <jason.santos@rangle.io>
2026-04-03 23:05:47 -0400

    fix(reports): Export DashboardData from model/dashboard

--- packages/reports/src/index.ts
+++ packages/reports/src/index.ts
@@ -1,4 +1,5 @@
 export * from './model/normalized.js';
+export * from './model/dashboard.js';
 export * from './model/history.js';
 export * from './connectors/interface.js';
 export * from './connectors/allure.js';
